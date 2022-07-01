b64bb20806c54e4c70eca0e665d6938a0073c4de
vemachakradhar <vemachakradhar@gmail.com>
2022-07-01 16:22:21 +0530

    first commit

---
+++ sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://13.232.132.138:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': '8cc5903f1a3b7ec268634aaf609048c2214e8e42',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
